fc3bde7e37e7dbf658a96d52b13de322b39ba48c
Joaquin Rivero <jq.rivero@gmail.com>
2025-07-10 13:59:05 -0700

    Fix accessibility issues in rnr block for MWPW-175506

--- acrobat/blocks/rnr/rnr.js
+++ acrobat/blocks/rnr/rnr.js
@@ -1,3 +1,4 @@
+/* rnr.js */
 /* eslint-disable func-names */
 /* eslint-disable compat/compat */
 /* eslint-disable max-len */
@@ -513,3 +514,5 @@ export default async function init(element) {
   await loadRnrData();
   initControls(element);
 }
+
+
